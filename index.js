2acd112648bdda3626aa15cc63b35e138603b4b8
Kelly Boettcher <kboettcher.kk@gmail.com>
2024-01-18 01:22:14 -0600

    Yeah

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+function writeCards(names, event) {
+    let newArray = [];
+    
+    for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+}
+    return newArray;
+}
+
+function countDown (){
+    let countD = 10;
+    while (countD > -1) {
+        console.log (countD--)
+    }
+}
